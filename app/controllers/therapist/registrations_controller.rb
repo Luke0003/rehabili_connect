dc6900b1e56ad7409754649b08fa4b6efe35eee3
EC2 Default User <ec2-user@ip-172-31-15-106.ap-northeast-1.compute.internal>
2022-11-05 13:02:15 +0000

    [Add]セラピスト追加機能

--- app/controllers/therapist/registrations_controller.rb
+++ app/controllers/therapist/registrations_controller.rb
@@ -2,7 +2,7 @@
 
 class Therapist::RegistrationsController < Devise::RegistrationsController
   layout "therapist_application"
-  # before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
@@ -39,12 +39,14 @@ class Therapist::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [
+      :first_name, :last_name, :first_name_kana, :last_name_kana, :email
+    ])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
