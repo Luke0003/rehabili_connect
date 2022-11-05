bfa72c669c60e313f7ede033f1526a3b348e7c0c
EC2 Default User <ec2-user@ip-172-31-15-106.ap-northeast-1.compute.internal>
2022-11-05 14:08:48 +0000

    [Add]ログアウト後などのpathを設定

--- app/controllers/therapist/registrations_controller.rb
+++ app/controllers/therapist/registrations_controller.rb
@@ -54,9 +54,10 @@ class Therapist::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+    # super(resource)
+    admin_root_path
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
