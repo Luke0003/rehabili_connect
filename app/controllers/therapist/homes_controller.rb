043b92411ab24b21cec4c7365cd5d389f094a497
EC2 Default User <ec2-user@ip-172-31-15-106.ap-northeast-1.compute.internal>
2022-11-06 05:23:59 +0000

    [Add]クライアント追加機能

--- app/controllers/therapist/homes_controller.rb
+++ app/controllers/therapist/homes_controller.rb
@@ -1,5 +1,7 @@
 class Therapist::HomesController < ApplicationController
   layout "therapist_application"
   def top
+    # 配列にページネーション機能を実装するために記述
+    @clients = Kaminari.paginate_array(current_therapist.clients).page(params[:page])
   end
 end
