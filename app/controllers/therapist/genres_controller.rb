66b263c4bb2acdffe20952aac31d08aa97b9ded8
EC2 Default User <ec2-user@ip-172-31-15-106.ap-northeast-1.compute.internal>
2022-11-06 04:01:55 +0000

    [Add]ジャンル機能

--- app/controllers/therapist/genres_controller.rb
+++ app/controllers/therapist/genres_controller.rb
@@ -1,17 +1,45 @@
 class Therapist::GenresController < ApplicationController
+  before_action :authenticate_therapist!
   layout "therapist_application"
   def index
+    @genre  = Genre.new
+    @genres = Genre.all
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
 
   def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      flash[:notice] = "ジャンルの登録に成功しました"
+      redirect_to therapist_genres_path
+    else
+      @genres = Genre.all
+      flash[:notice] = "ジャンルの登録に失敗しました"
+      render :index
+    end
   end
 
   def update
+    @genre = Genre.find(params[:id])
+    if @genre.update(genre_params)
+      flash[:notice] = "ジャンルの更新に成功しました"
+      redirect_to therapist_genres_path
+    else
+      flash[:notice] = "ジャンルの更新に失敗しました"
+      render :edit
+    end
   end
 
   def destroy
   end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:genre_name)
+  end
+
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,5 @@
 class Genre < ApplicationRecord
   has_many :menus
+
+  validates :genre_name, presence: true, uniqueness: true
 end
