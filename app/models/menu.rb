c4d7e25e4a0e9b272d8fba6883c74d1fd55bd26b
EC2 Default User <ec2-user@ip-172-31-15-106.ap-northeast-1.compute.internal>
2022-11-07 03:19:01 +0000

    [Add]メニュー投稿機能

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,4 +1,9 @@
 class Menu < ApplicationRecord
   belongs_to :genre
   has_many :client_menus
+  has_one_attached :menu_video
+
+  validates :menu_name, presence: true, uniqueness: true
+  validates :menu_content, presence: true
+  validates :menu_purpose, presence: true
 end
