11de798961392893c61b6ea76bd0c26839211559
EC2 Default User <ec2-user@ip-172-31-15-106.ap-northeast-1.compute.internal>
2022-11-08 12:31:06 +0000

    [Add]記録機能完成

--- app/controllers/public/client_records_controller.rb
+++ app/controllers/public/client_records_controller.rb
@@ -4,7 +4,9 @@ class Public::ClientRecordsController < ApplicationController
 
   def show
     @day = params[:id].to_date
-    @client_record = ClientRecord.new
+
+    @client_record = ClientRecord.find_by(record_date: @day)
+    @client_record = ClientRecord.new if @client_record.nil?
     @client_menus = current_client.client_menus.where(start_date: @day)
   end
 
@@ -18,14 +20,23 @@ class Public::ClientRecordsController < ApplicationController
   def create
     @client_record = current_client.client_records.new(client_record_params)
     @client_record.save
+
+    # check_box_tagのデータをkey, valueで分けて繰り返し、
+    # ClientMenuのis_completedを更新する
+    params[:client_menus].each do |key, value|
+      ClientMenu.find(key).update(is_completed: value, client_record_id: @client_record.id)
+    end
+
     redirect_to client_records_path
   end
 
   def update
-  end
-
-  # クライアントメニューの実施記録を更新
-  def client_menu_update
+    @client_record = ClientRecord.find_by(record_date: client_record_params[:record_date])
+    @client_record.update(client_record_params)
+    params[:client_menus].each do |key, value|
+      ClientMenu.find(key).update(is_completed: value,  client_record_id: @client_record.id)
+    end
+    redirect_to client_records_path
   end
 
   private
