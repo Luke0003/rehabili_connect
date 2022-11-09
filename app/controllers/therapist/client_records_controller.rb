a919d4f22515e80907bc964c7815674f567f7a04
EC2 Default User <ec2-user@ip-172-31-15-106.ap-northeast-1.compute.internal>
2022-11-09 05:14:31 +0000

    [Add]記録表示機能

--- app/controllers/therapist/client_records_controller.rb
+++ app/controllers/therapist/client_records_controller.rb
@@ -2,8 +2,13 @@ class Therapist::ClientRecordsController < ApplicationController
   before_action :authenticate_therapist!
   layout "therapist_application"
   def index
+    @client = Client.find(params[:client_id])
+    @client_records = ClientRecord.page(params[:page])
   end
 
   def show
+    @client_record = ClientRecord.find(params[:id])
+    @day = @client_record.record_date
   end
+
 end
