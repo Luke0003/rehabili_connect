6b765529a792cf022c4d51ecc18c732e5cde74bf
EC2 Default User <ec2-user@ip-172-31-15-106.ap-northeast-1.compute.internal>
2022-11-16 05:08:45 +0000

    [Add]退会機能

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :client_state, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -18,7 +19,17 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+
+  # クライアントへのサービスがすでに終了しているかを確認
+  def client_state
+    @client = Client.find_by(email: params[:client][:email])
+    return if !@client
+    if (@client.valid_password?(params[:client][:password])) && (@client.is_deleted == true)
+      flash[:notice] = "サービスは終了いたしました"
+      redirect_to new_client_session_path
+    end
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
--- app/controllers/therapist/sessions_controller.rb
+++ app/controllers/therapist/sessions_controller.rb
@@ -3,6 +3,7 @@
 class Therapist::SessionsController < Devise::SessionsController
   layout "therapist_application"
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :therapist_state, only: [:create]
 
   def after_sign_out_path_for(resource)
      new_therapist_session_path
@@ -23,7 +24,17 @@ class Therapist::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+
+   # セラピストへのサービスがすでに終了しているかを確認
+  def therapist_state
+    @therapist = Therapist.find_by(email: params[:therapist][:email])
+    return if !@therapist
+    if (@therapist.valid_password?(params[:therapist][:password])) && (@therapist.is_deleted == true)
+      flash[:notice] = "退職済みのため、ご利用いただけません"
+      redirect_to new_therapist_session_path
+    end
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
