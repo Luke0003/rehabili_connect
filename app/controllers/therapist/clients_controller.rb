e3d4f8c639444616c98b1dc35ee49b8c8be4b2e1
EC2 Default User <ec2-user@ip-172-31-15-106.ap-northeast-1.compute.internal>
2022-11-17 07:46:52 +0000

    [Add]セラピスト側のレイアウト

--- app/controllers/therapist/clients_controller.rb
+++ app/controllers/therapist/clients_controller.rb
@@ -4,4 +4,24 @@ class Therapist::ClientsController < ApplicationController
   def show
     @client = Client.find(params[:id])
   end
+
+  def edit
+    @client = Client.find(params[:id])
+  end
+
+  def update
+    @client = Client.find(params[:id])
+    if @client.update(client_params)
+      flash[:notice] = "クライアント情報の更新に成功しました"
+      redirect_to therapist_client_path(@client)
+    else
+      flash[:notice] = "クライアント情報の更新に失敗しました"
+      render :edit
+    end
+  end
+
+  private
+  def client_params
+    params.require(:client).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :purpose, :email, :is_deleted)
+  end
 end
