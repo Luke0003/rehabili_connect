3b4b5d985cd622bfb8338e8ef08d52afcb0ab300
EC2 Default User <ec2-user@ip-172-31-15-106.ap-northeast-1.compute.internal>
2022-12-13 10:11:37 +0000

    [Clean]下位問題のメソッド化

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,11 +2,7 @@ class Public::HomesController < ApplicationController
   before_action :authenticate_client!
   def top
     @client_menus = current_client.client_menus
-    @client_records = current_client.client_records
-    # キーを登録記録日、バリューを体調としたハッシュを取得
-    @conditions = {}
-    @client_records.each do |client_record|
-      @conditions[client_record.record_date] = client_record.condition
-    end
+    # カレンダーに紐付けることを目的とし、ハッシュを生成
+    @conditions = ClientRecord.gen_hash_k_record_date_v_condition(current_client)
   end
 end
