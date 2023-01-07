0af35a8966025d26cea01bd9dcfb08f6b97566e6
EC2 Default User <ec2-user@ip-172-31-15-106.ap-northeast-1.compute.internal>
2023-01-07 05:03:02 +0000

    [Add]本日の予定をtop画面で表示

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,6 +2,8 @@ class Public::HomesController < ApplicationController
   before_action :authenticate_client!
   def top
     @client_menus = current_client.client_menus
+    today = Time.zone.today
+    @today_client_menus = @client_menus.where(start_date: today)
     # カレンダーに紐付けることを目的とし、ハッシュを生成
     @conditions = ClientRecord.gen_hash_k_record_date_v_condition(current_client)
     # gem 'gon'を利用してapplication.jsに値を渡している
