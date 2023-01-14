5858f9c673a8715b097336c5cb1fe881b500b230
EC2 Default User <ec2-user@ip-172-31-15-106.ap-northeast-1.compute.internal>
2023-01-14 23:57:54 +0900

    [Add]セラピスト側のソケット通信

--- app/controllers/public/chats_controller.rb
+++ app/controllers/public/chats_controller.rb
@@ -20,11 +20,11 @@ class Public::ChatsController < ApplicationController
       @chat = current_client.chats.new(chat_params)
       @chat.save!
       notification = current_client.notifications.new(chat_id: @chat.id, therapist_id: current_client.therapist.id, checked_client: true)
-      ActionCable.server.broadcast 'chat_channel', {content: @chat, created_at: @chat.created_at.strftime('%m/%d %H:%M')} if notification.save!
+      ActionCable.server.broadcast 'chat_channel', {user: 1, content: @chat, created_at: @chat.created_at.strftime('%m/%d %H:%M')} if notification.save!
     end
   rescue => e
     puts "クライアントチャットエラー: #{e}"
-    render :validater
+    # render :validater
   end
 
   private
--- app/javascript/channels/chat_channel.js
+++ app/javascript/channels/chat_channel.js
@@ -11,25 +11,48 @@ consumer.subscriptions.create("ChatChannel", {
 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
-    const clientImage = document.getElementById('client_image');
-    const html = `
-    <div class="d-flex justify-content-end">
-      <img src="${clientImage["src"]}" class="mb-2 rounded-circle">
-    </div>
-    <div class="d-flex justify-content-end">
-      <p class="text-left my_chat mr-5 mb-0">
-        ${data.content.message}
-      </p>
-    </div>
-    <div class="d-flex justify-content-end">
-      <p class="text-left mr-5">
-        <span class="created_at">${data.created_at}</span>
-      </p>
-    </div>`;
-    const messages = document.getElementById('messages_all');
-    const newMessage = document.getElementById('chat_message');
-    messages.insertAdjacentHTML('beforeend', html);
-    newMessage.value = '';
-    $(".chat_btn").removeAttr("disabled");
+    if (data.user == 1 ) {
+      const clientImage = document.getElementById('client_image');
+      const html = `
+      <div class="d-flex justify-content-end">
+        <img src="${clientImage["src"]}" class="mb-2 rounded-circle">
+      </div>
+      <div class="d-flex justify-content-end">
+        <p class="text-left my_chat mr-5 mb-0">
+          ${data.content.message}
+        </p>
+      </div>
+      <div class="d-flex justify-content-end">
+        <p class="text-left mr-5">
+          <span class="created_at">${data.created_at}</span>
+        </p>
+      </div>`;
+      const messages = document.getElementById('messages_all');
+      const newMessage = document.getElementById('chat_message');
+      messages.insertAdjacentHTML('beforeend', html);
+      newMessage.value = '';
+      $(".chat_btn").removeAttr("disabled");
+    }else{
+      const therapistImage = document.getElementById('therapist_image');
+      const html = `
+      <div class="d-flex justify-content-end">
+        <img src="${therapistImage["src"]}" class="mb-2 rounded-circle">
+      </div>
+      <div class="d-flex justify-content-end">
+        <p class="text-left my_chat mr-5 mb-0">
+          ${data.content.message}
+        </p>
+      </div>
+      <div class="d-flex justify-content-end">
+        <p class="text-left mr-5">
+          <span class="created_at">${data.created_at}</span>
+        </p>
+      </div>`;
+      const messages = document.getElementById('messages_all');
+      const newMessage = document.getElementById('chat_message');
+      messages.insertAdjacentHTML('beforeend', html);
+      newMessage.value = '';
+      $(".chat_btn").removeAttr("disabled");
+    }
   }
 });
