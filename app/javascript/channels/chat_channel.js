84a574a529ce5e499118a3396326600770debefd
EC2 Default User <ec2-user@ip-172-31-15-106.ap-northeast-1.compute.internal>
2023-01-15 15:19:50 +0900

    [Add]セラピスト側のjs記述の追加

--- app/javascript/channels/chat_channel.js
+++ app/javascript/channels/chat_channel.js
@@ -16,19 +16,20 @@ consumer.subscriptions.create("ChatChannel", {
       const chatPartnerName = document.getElementById('chat_partner_name');
       if (data.chat_partner_name == chatPartnerName.innerHTML) {
         const html = `
-        <div class="d-flex justify-content-end">
-          <img src="${clientImage["src"]}" class="mb-2 rounded-circle">
-        </div>
-        <div class="d-flex justify-content-end">
-          <p class="text-left my_chat mr-5 mb-0">
-            ${data.content.message}
-          </p>
-        </div>
-        <div class="d-flex justify-content-end">
-          <p class="text-left mr-5">
-            <span class="created_at">${data.created_at}</span>
-          </p>
-        </div>`;
+          <div class="d-flex justify-content-end">
+            <img src="${clientImage["src"]}" class="mb-2 rounded-circle">
+          </div>
+          <div class="d-flex justify-content-end">
+            <p class="text-left my_chat mr-5 mb-0">
+              ${data.content.message}
+            </p>
+          </div>
+          <div class="d-flex justify-content-end">
+            <p class="text-left mr-5">
+              <span class="created_at">${data.created_at}</span>
+            </p>
+          </div>`;
+
         const messages = document.getElementById('messages_all');
         const newMessage = document.getElementById('chat_message');
         messages.insertAdjacentHTML('beforeend', html);
@@ -36,19 +37,19 @@ consumer.subscriptions.create("ChatChannel", {
         $(".chat_btn").removeAttr("disabled");
       }else{
         const html = `
-        <div class="d-flex justify-content-start">
-          <img src="${clientImage["src"]}" class="mb-2 rounded-circle" %>
-        </div>
-        <div class="d-flex justify-content-start">
-          <p class="another_chat ml-5 mb-0">
-            ${data.content.message}
-          </p>
-        </div>
-        <div class="d-flex justify-content-start">
-          <p class="text-left ml-5">
-            <span class="created_at">${data.created_at}</span>
-          </p>
-        </div>`;
+          <div class="d-flex justify-content-start">
+            <img src="${clientImage["src"]}" class="mb-2 rounded-circle" %>
+          </div>
+          <div class="d-flex justify-content-start">
+            <p class="another_chat ml-5 mb-0">
+              ${data.content.message}
+            </p>
+          </div>
+          <div class="d-flex justify-content-start">
+            <p class="text-left ml-5">
+              <span class="created_at">${data.created_at}</span>
+            </p>
+          </div>`;
 
         const messages = document.getElementById('messages_all');
         const newMessage = document.getElementById('chat_message');
@@ -58,25 +59,50 @@ consumer.subscriptions.create("ChatChannel", {
       }
     }else{
       const therapistImage = document.getElementById('therapist_image');
-      const html = `
-      <div class="d-flex justify-content-end">
-        <img src="${therapistImage["src"]}" class="mb-2 rounded-circle">
-      </div>
-      <div class="d-flex justify-content-end">
-        <p class="text-left my_chat mr-5 mb-0">
-          ${data.content.message}
-        </p>
-      </div>
-      <div class="d-flex justify-content-end">
-        <p class="text-left mr-5">
-          <span class="created_at">${data.created_at}</span>
-        </p>
-      </div>`;
-      const messages = document.getElementById('messages_all');
-      const newMessage = document.getElementById('chat_message');
-      messages.insertAdjacentHTML('beforeend', html);
-      newMessage.value = '';
-      $(".chat_btn").removeAttr("disabled");
+      const chatPartnerName = document.getElementById('chat_partner_name');
+      if (data.chat_partner_name == chatPartnerName.innerHTML) {
+        const html = `
+          <div class="d-flex justify-content-end">
+            <img src="${therapistImage["src"]}" class="mb-2 rounded-circle">
+          </div>
+          <div class="d-flex justify-content-end">
+            <p class="text-left my_chat mr-5 mb-0">
+              ${data.content.message}
+            </p>
+          </div>
+          <div class="d-flex justify-content-end">
+            <p class="text-left mr-5">
+              <span class="created_at">${data.created_at}</span>
+            </p>
+          </div>`;
+
+        const messages = document.getElementById('messages_all');
+        const newMessage = document.getElementById('chat_message');
+        messages.insertAdjacentHTML('beforeend', html);
+        newMessage.value = '';
+        $(".chat_btn").removeAttr("disabled");
+      }else{
+        const html = `
+          <div class="d-flex justify-content-start">
+            <img src="${therapistImage["src"]}" class="mb-2 rounded-circle">
+          </div>
+          <div class="d-flex justify-content-start">
+            <p class="another_chat ml-5 mb-0">
+               ${data.content.message}
+            </p>
+          </div>
+          <div class="d-flex justify-content-start">
+            <p class="text-left ml-5">
+              <span class="created_at">${data.created_at}</span>
+            </p>
+          </div>`;
+
+        const messages = document.getElementById('messages_all');
+        const newMessage = document.getElementById('chat_message');
+        messages.insertAdjacentHTML('beforeend', html);
+        newMessage.value = '';
+        $(".chat_btn").removeAttr("disabled");
+      }
     }
   }
 });
