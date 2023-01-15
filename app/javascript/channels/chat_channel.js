9fe670489bf2ea9fda4fd433628cddd5162fda1d
EC2 Default User <ec2-user@ip-172-31-15-106.ap-northeast-1.compute.internal>
2023-01-15 14:53:47 +0900

    [Fix]jsファイルの修正

--- app/javascript/channels/chat_channel.js
+++ app/javascript/channels/chat_channel.js
@@ -16,27 +16,6 @@ consumer.subscriptions.create("ChatChannel", {
       const chatPartnerName = document.getElementById('chat_partner_name');
       if (data.chat_partner_name == chatPartnerName.innerHTML) {
         const html = `
-        <div class="d-flex justify-content-start">
-          <%= image_tag @therapist.get_therapist_image(50, 50), id: "therapist_image", class: "mb-2 rounded-circle" %>
-        </div>
-        <div class="d-flex justify-content-start">
-          <p class="another_chat ml-5 mb-0">
-            <%= safe_join(chat.message.split("\n"),tag(:br)) %>
-          </p>
-        </div>
-        <div class="d-flex justify-content-start">
-          <p class="text-left ml-5">
-            <span class="created_at"><%= chat.created_at.strftime('%m/%d %H:%M') %></span>
-          </p>
-        </div>`;
-
-        const messages = document.getElementById('messages_all');
-        const newMessage = document.getElementById('chat_message');
-        messages.insertAdjacentHTML('beforeend', html);
-        newMessage.value = '';
-        $(".chat_btn").removeAttr("disabled");
-      }else{
-        const html = `
         <div class="d-flex justify-content-end">
           <img src="${clientImage["src"]}" class="mb-2 rounded-circle">
         </div>
@@ -50,6 +29,27 @@ consumer.subscriptions.create("ChatChannel", {
             <span class="created_at">${data.created_at}</span>
           </p>
         </div>`;
+        const messages = document.getElementById('messages_all');
+        const newMessage = document.getElementById('chat_message');
+        messages.insertAdjacentHTML('beforeend', html);
+        newMessage.value = '';
+        $(".chat_btn").removeAttr("disabled");
+      }else{
+        const html = `
+        <div class="d-flex justify-content-start">
+          <img src="${clientImage["src"]}" class="mb-2 rounded-circle" %>
+        </div>
+        <div class="d-flex justify-content-start">
+          <p class="another_chat ml-5 mb-0">
+            ${data.content.message}
+          </p>
+        </div>
+        <div class="d-flex justify-content-start">
+          <p class="text-left ml-5">
+            <span class="created_at">${data.created_at}</span>
+          </p>
+        </div>`;
+
         const messages = document.getElementById('messages_all');
         const newMessage = document.getElementById('chat_message');
         messages.insertAdjacentHTML('beforeend', html);
