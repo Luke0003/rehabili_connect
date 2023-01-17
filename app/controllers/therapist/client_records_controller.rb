e6524255a0a95acfd35f7fa4f9fff6948e5c1727
EC2 Default User <ec2-user@ip-172-31-15-106.ap-northeast-1.compute.internal>
2023-01-17 17:35:43 +0900

    [Add]リハビリ記録時間の前日比と合計時間の表示

--- app/controllers/therapist/client_records_controller.rb
+++ app/controllers/therapist/client_records_controller.rb
@@ -6,6 +6,7 @@ class Therapist::ClientRecordsController < ApplicationController
     @client_menus = @client.client_menus
     # カレンダーに紐付けることを目的とし、ハッシュを生成
     @conditions = ClientRecord.gen_hash_k_record_date_v_condition(@client)
+    @total_rehabili_time = RehabiliTime.total_rehabili_time(@client)
   end
 
   def show
@@ -13,6 +14,13 @@ class Therapist::ClientRecordsController < ApplicationController
     client = Client.find(params[:client_id])
     @client_record = client.client_records.find_by(record_date: @day)
     @rehabili_time_of_the_day = @client_record.get_rehabili_time_of_the_day_for_display
+
+     # 前日比を取得
+    the_day_rehabili_time = RehabiliTime.array_rehabili_time_of_the_day(client, @day)
+    the_day_before_rehabili_time = RehabiliTime.array_rehabili_time_of_the_day(client, @day.ago(1.days))
+    the_day_rehabili_time_sec = RehabiliTime.rehabili_time_to_sec(the_day_rehabili_time).to_f
+    the_day_before_rehabili_time_sec = RehabiliTime.rehabili_time_to_sec(the_day_before_rehabili_time).to_f
+    @the_day_before_per = RehabiliTime.per_of_two_day(the_day_rehabili_time_sec, the_day_before_rehabili_time_sec)
   end
 
 end
